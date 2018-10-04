de37649ec7fbdc77f4f5f74481c8f8a7c7b6c29a
Miguel Gomes <gomesmike24@hotmail.com>
2018-10-04 14:34:36 +0100

    "Foi enviado apartir do VS"

--- VSHelloWorld_130221100/Program.cs
+++ VSHelloWorld_130221100/Program.cs
@@ -14,6 +14,7 @@ namespace VSHelloWorld_130221100
     {
         public static void Main(string[] args)
         {
+            //Isto é agora um teste de como partilhar ficheiros
             CreateWebHostBuilder(args).Build().Run();
         }
 
